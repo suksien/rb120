fea61fdbebc5075f05a9d24328af53c1fd7828a6
suksien <hahasuksien@yahoo.com.my>
2023-12-07 21:10:23 +0800

    starting exercises for 120

---
+++ exercises/oo1/4.rb
@@ -0,0 +1,14 @@
+=begin
+
+Using the code from the previous exercise, add an #initialize method that prints I'm a cat! 
+when a new Cat object is instantiated.
+
+=end
+
+class Cat
+  def initialize
+    puts "I'm a cat!"
+  end
+end
+
+kitty = Cat.new

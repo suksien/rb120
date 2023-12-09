5334e87c5ac687b82e93bb30da80c427c0d854ec
suksien <hahasuksien@yahoo.com.my>
2023-12-09 13:53:15 +0800

    completed oo accessor methods exercises

---
+++ exercises/oo_accessor/9.rb
@@ -0,0 +1,20 @@
+=begin
+
+Using the following code, multiply @age by 2 upon assignment, then multiply @age by 2 
+again when @age is returned by the getter method.
+
+=end
+
+class Person
+  def age=(age)
+    @age = age * 2
+  end
+
+  def age
+    @age * 2
+  end
+end
+
+person1 = Person.new
+person1.age = 20
+puts person1.age

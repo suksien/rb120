8cb667b4820702358c5526520359f37ca0d95f2c
suksien <hahasuksien@yahoo.com.my>
2023-12-18 22:09:11 +0800

    finishing practice problems: hard 1

---
+++ lesson4/pp_hard1/1.rb
@@ -0,0 +1,56 @@
+module Fuelable
+  def range # do you call this an instance method? can one define a class method in a module?
+    fuel_capacity * fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Fuelable
+  attr_accessor :speed, :heading
+  attr_reader :fuel_efficiency, :fuel_capacity
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    @fuel_efficiency = km_traveled_per_liter
+    @fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Catamaran
+  include Fuelable
+  attr_reader :propeller_count, :hull_count
+  attr_accessor :speed, :heading
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    # ... code omitted ...
+  end
+end
+
+auto = Auto.new
+p auto.range
+
+motorbike = Motorcycle.new
+p motorbike.range
